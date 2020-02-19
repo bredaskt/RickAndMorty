1e5df8439b98947d98f2bee81e855842ef134483
= <=>
2020-02-19 09:06:45 -0300

    Partial implementation service rick and morty.

---
+++ app/src/main/java/com/example/myapplication/model/Characters.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication.model
+
+    class CharacterRM(val name: String, val species: String)
+
+    class Characters(val results: List<CharacterRM>)
+
---
+++ app/src/main/java/com/example/myapplication/model/CharactersService.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.model
+
+import android.telecom.Call
+import retrofit2.http.GET
+
+interface CharacterService {
+//    @GET("character")
+//    fun list() : Call<Characters>
+}
