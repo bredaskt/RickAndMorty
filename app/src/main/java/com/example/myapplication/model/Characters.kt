58a46655a8910fe72530600dd264fba47f35439f
= <=>
2020-02-20 15:56:55 -0300

    Partial implementation service rick and morty.

--- app/src/main/java/com/example/myapplication/model/Characters.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication.model
-
-    class CharacterRM(val name: String, val species: String)
-
-    class Characters(val results: List<CharacterRM>)
-
--- app/src/main/java/com/example/myapplication/model/CharactersService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.model
-
-import android.telecom.Call
-import retrofit2.http.GET
-
-interface CharacterService {
-//    @GET("character")
-//    fun list() : Call<Characters>
-}
